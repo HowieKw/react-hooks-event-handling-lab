c966ef43193362ad78809fdc9968b2f7ab0026d6
Howard Kwon <howie.ykwon70@gmail.com>
2021-08-12 13:06:55 -0400

    commit tree b167b194d67a078a0fef395d5e59711940ffd44d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+
+const EyesOnMe = () => {
+
+    function onFocusHandler() {
+        console.log('Good!')
+    }
+
+    function onBlurHandler() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={onFocusHandler} onBlur={onBlurHandler}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
+
+
+const Keypad = () => {
+
+    function handleOnChange() {
+        console.log('Entering password...')
+    }
+
+    return(
+        
+        <input type='password' onChange={handleOnChange}></input>
+    )
+}
+
+export default Keypad;
